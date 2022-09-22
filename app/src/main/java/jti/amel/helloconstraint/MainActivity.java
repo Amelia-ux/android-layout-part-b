e5e957a1c5401aaa2ff618312cc596b3f63e5064
amelia <meliamel.9977@gmail.com>
2022-09-22 23:00:55 +0700

    homework

--- app/src/main/java/jti/amel/helloconstraint/MainActivity.java
+++ app/src/main/java/jti/amel/helloconstraint/MainActivity.java
@@ -1,31 +1,60 @@
 package jti.amel.helloconstraint;
 
+import android.graphics.Color;
 import android.widget.Toast;
 import android.widget.TextView;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private int mCount =0;
+
+    private int mCount = 0;
     private TextView mShowCount;
+    private Button mButtonZero;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mShowCount = (TextView) findViewById(R.id.show_count);
-    }
 
+        mShowCount = (TextView)findViewById(R.id.show_count);
+        mButtonZero = (Button)findViewById(R.id.button_zero);
+    }
     public void showToast(View view) {
-        Toast toast = Toast.makeText(this, R.string.toast_message,
-                Toast.LENGTH_SHORT);
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+
         toast.show();
     }
-
     public void countUp(View view) {
-        mCount++;
-        if (mShowCount!=null){
+        ++mCount;
+        if(mShowCount!=null){
+            mButtonZero.setBackgroundColor(getResources().getColor(R.color.purple_500));
+            if(mCount%2 == 0){
+                view.setBackgroundColor(getResources().getColor(R.color.purple_200));
+            }
+            else{
+                view.setBackgroundColor(getResources().getColor(R.color.blue));
+            }
             mShowCount.setText(Integer.toString(mCount));
+
+            setColor(view, Color.GREEN);
+            setColor(findViewById(R.id.button_zero), Color.MAGENTA);
         }
+
+    }
+    public void zeroCount(View view)
+    {
+        mCount = 0;
+        mShowCount.setText("0");
+
+        setColor(view, Color.parseColor("#ffaaaaaa"));
+        setColor(findViewById(R.id.button_count), Color.BLUE);
+    }
+    private void setColor(View view, int color)
+    {
+        view.setBackgroundColor(color);
     }
 }
